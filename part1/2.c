01fdd747feed6d18513fb02b63194c07a4b9222c
backFox <jorealbusiness@gmail.com>
2022-08-14 16:21:25 +0900

    #1 - Part 1 완독
    예제 01 ~ 10번까지 공부하며 기초를 공부한다.

---
+++ part1/2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main() {
+    int x;
+    int y;
+    int z;
+
+    x = 1;
+    y = 2;
+
+    z = x + y;
+
+    printf("%d", z);
+}
---
+++ part1/3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define X 1
+#define PI 3.141592
+
+main()
+{
+    double z;
+    z = X + PI;
+    printf("%f", z);
+}
